b75c602510fd259c9cd22878664e25bae0189e36
Raj Muni <rockingraj8247@gmail.com>
2024-10-12 18:40:33 +0530

    added more margin

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,6 +1,6 @@
 export default function Footer()
 {
-   return <section className="text-white text-center mt-8 h-auto flex items-center justify-center w-[100vw] bg-[#FC9800]">
+   return <section className="text-white text-center mt-14 h-auto flex items-center justify-center w-[100vw] bg-[#FC9800]">
         <div className="limit max-w-[1200px] float-start">
             <div className="founders">
                 <h2>Founders</h2>
