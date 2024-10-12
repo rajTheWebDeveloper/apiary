4740362eef281628ca8e2e1f01652bf2319506c1
Raj Muni <rockingraj8247@gmail.com>
2024-10-12 18:38:16 +0530

    added founders

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,7 +1,14 @@
 export default function Footer()
 {
    return <section className="text-white text-center mt-8 h-auto flex items-center justify-center w-[100vw] bg-[#FC9800]">
-        <div className="limit max-w-[1200px] flex justify-center float-start">
+        <div className="limit max-w-[1200px] float-start">
+            <div className="founders">
+                <h2>Founders</h2>
+                <div className="list flex justify-center gap-4">
+                    <h3>Manoj Kumar</h3>
+                    <h3>Muni Babu</h3>
+                </div>
+            </div>
             <h2>&copy; M&M Apiaries All rights are reserved 2024</h2>
         </div>
    </section>
